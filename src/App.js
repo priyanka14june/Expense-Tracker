d76d9bcf72bb8c2bca57d3b69f70a4fd18798dff
priyanka14june <106279888+priyanka14june@users.noreply.github.com>
2023-03-09 13:38:01 +0100

    props concept

--- src/App.js
+++ src/App.js
@@ -3,14 +3,53 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 /* <ExpenseItem /> can be written as <ExpenseItem> </ExpenseItem> and every componenet should be imported to app.js as its the main componenet to show up on page */
+/* new Date() is the JS function to fect date. new Date(2021, 2, 28) is the date set with initaial date as 2021 as year,  month as march(02--because month starts from 0(jan) and date as 28th)*/
+const expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
   return (
     <div>
       <h2>Money Tracker</h2>
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
