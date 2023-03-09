e7e180640a35e0243b87f3eed8481d832eb117f5
priyanka14june <106279888+priyanka14june@users.noreply.github.com>
2023-03-09 13:41:01 +0100

    Expense item props (#2)
    * props concept
    
    * props concept

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  /* new Date() is the JS function to fect date. new Date(2021, 2, 28) is the date set with initaial date as 2021 as year,  month as march(02--because month starts from 0(jan) and date as 28th)*/
+function ExpenseItem(props) {
   /* Naming convection of variable name starts with small letters and then capital letter as expenseDate */
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insaurance";
-  const expenseAmount = 294.79;
 
   return (
     /* you must have only 1 root element per return statement */
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <h2 className="expense-item__description">{expenseTitle}</h2>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div>{props.date.toISOString()}</div>
+      <h2 className="expense-item__description">{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
