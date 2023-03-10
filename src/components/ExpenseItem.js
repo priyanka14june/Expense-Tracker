3dbc96cda8dd21f3b3e57ad9b2ece95fd88c99f5
priyanka14june <106279888+priyanka14june@users.noreply.github.com>
2023-03-10 16:10:23 +0100

    card component created as wrapper (#8)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   /* Naming convection of variable name starts with small letters and then capital letter as expenseDate */
 
   return (
     /* you must have only 1 root element per return statement */
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <h2 className="expense-item__description">{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
