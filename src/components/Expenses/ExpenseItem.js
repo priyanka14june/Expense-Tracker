39b78f9b549d9408e3a7e832d2a40ce47384d440
priyanka14june <106279888+priyanka14june@users.noreply.github.com>
2023-03-12 13:31:11 +0100

    useState() hook is introduce

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   /* Naming convection of variable name starts with small letters and then capital letter as expenseDate */
-
+  /* const [1st value, 2nd value] = useState(1st value) - first value is the initial value of variable and the 2nd value is the value you want to change */
+  const [title, setTitle] = useState(props.title);
+  function clickHandler() {
+    return setTitle("updated");
+  }
   return (
     /* you must have only 1 root element per return statement */
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
-      <h2 className="expense-item__description">{props.title}</h2>
+      <h2 className="expense-item__description">{title}</h2>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
